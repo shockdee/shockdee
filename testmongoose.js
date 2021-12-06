5d1f3e957ef1aa18fc8bac6fcfea90ef8c1f7f1e
shockdee <1062740339@qq.com>
2021-12-05 16:24:54 -0800

    First commit

---
+++ testmongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const schema = {
+    name: String,
+    age: Number,
+    health: String
+}
+const Cat = mongoose.model('Cat', schema);
+
+// const kitty = new Cat({ name: 'testZildjian' });
+// kitty.save().then(() => console.log('testmeow'));
+// const kitty1 = new Cat({name:'zhang1',age:1,helth:'good'});
+// kitty1.save().then(()=>console.log('new schema writed'));
+Cat.find({name:'zhang3'},(err,data)=>{console.log(data[0]._doc.name)})
